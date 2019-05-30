418840f292ad3ce96e4af62f718b65a73524e5b7
milkkanz <justinkanz@gmail.com>
2019-05-30 01:01:26 -0400

    Added Cieling,Collison Volumes, and Door Timers

--- EscapeRoom/Source/EscapeRoom/OpenDoor.h
+++ EscapeRoom/Source/EscapeRoom/OpenDoor.h
@@ -22,18 +22,25 @@ protected:
 	virtual void BeginPlay() override;
 
 	void OpenDoor();
+	void CloseDoor();
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-	UPROPERTY(VisibleAnywhere)
-		float OpenAngle = 90.0f;
+	UPROPERTY(EditAnywhere)
+		float OpenAngle = -90.0f;
 
 	UPROPERTY(EditAnywhere)
 		ATriggerVolume* PressurePlate;
 
 	UPROPERTY(EditAnywhere)
+		float DoorCloseDelay = 1.f;
+
+		float LastDoorOpenTime;
+
 		AActor* ActorThatOpens; //Remember Pawn Inherits from Actor
+		AActor* Owner; //The owning Door
+		
 };
